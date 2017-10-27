123344be1dead81f5491969d2b7fe047d7a83320
asiaat <kalle@aasia.ee>
2017-10-28 00:29:05 +0300

    Feedly api reading one feed and parsing it into json output

--- feedlymngr/feedlymngr.py
+++ feedlymngr/feedlymngr.py
@@ -1,5 +1,6 @@
 import configparser
 from feedly.client import FeedlyClient
+import datetime
 
 class FeedlyMngr(object):
 
@@ -40,10 +41,52 @@ class FeedlyMngr(object):
 
         return categories
 
+    def feeds_from_category(self,inp_cat):
+
+        result = self.__feedly.get_feed_content(self.__FEEDLY_CLIENT_SECRET,
+                                                inp_cat.get('id'), False)
+
+        for i in result.get('items', []):
+            #print i
+            self.__parse_feed(i)
+
+
+    def __parse_feed(self,inp_feed):
+
+        try:    title = inp_feed['title']
+        except: title = ""
+
+        try:    author = inp_feed['author']
+        except: author = ""
+
+        try:    content = inp_feed['summary']['content']
+        except: content = ""
+
+        time_stamp = datetime.datetime.fromtimestamp(int(str(inp_feed['published'])[:10])).strftime("%Y-%m-%dT%H:%M:%SZ")
+        #print("timestamp:\t", time_stamp)
+
+        json_data = {
+            'originid':         inp_feed['originId'],
+            'type':             inp_feed['alternate'][0]['type'],
+            'origin_url':       inp_feed['origin']['htmlUrl'],
+            'origin_title':     inp_feed['origin']['title'],
+            'title':            title,
+            'author':           author,
+            'fdl_category':     inp_feed['categories'][0]['label'],
+            'timestamp': "%s" % time_stamp,
+            'content':          content,
+        }
+        print(json_data)
+
+
+
+
 if __name__ == '__main__':
 
     fm = FeedlyMngr('../conf/feedly.conf')
     print "helo"
 
-    for c in fm.feedly_categories():
-        print c
+
+    for category  in fm.feedly_categories()[1:6]:
+        print fm.feeds_from_category(category)
+        print  category
